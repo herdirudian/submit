c74e263b52a948ca7385a16e670ee57d7beb7801
herdirudian <herdirudian@users.noreply.github.com>
2026-07-28 00:01:45 +0700

    security: add missing HTTP security headers to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Terapkan ke semua route di aplikasi
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:; img-src 'self' blob: data: https: http:; font-src 'self' data: https:; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'self'; connect-src 'self' https: http: wss: ws:;",
+          }
+        ],
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
