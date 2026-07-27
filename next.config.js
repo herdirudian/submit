30031ccb5f50f868a8c97d0fdb63eca42e262d66
herdirudian <herdirudian@users.noreply.github.com>
2026-07-28 00:12:12 +0700

    security: remove unsafe-eval from CSP to improve security score

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:; img-src 'self' blob: data: https: http:; font-src 'self' data: https:; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'self'; connect-src 'self' https: http: wss: ws:;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:; img-src 'self' blob: data: https: http:; font-src 'self' data: https:; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'self'; connect-src 'self' https: http: wss: ws:;",
           }
         ],
       },
